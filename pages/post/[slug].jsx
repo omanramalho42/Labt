35982b4cfcb461869ec6276ec7ee3b1feb565a0b
= <=>
2022-11-10 14:22:19 -0300

    creating components and view id post

---
+++ pages/post/[slug].jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { getPosts, getPostDetails } from '../../services'
+
+import { 
+  PostDetail, 
+  Categories, 
+  PostWidget, 
+  Author, 
+  Comments, 
+  CommentsForm 
+} from '../../components'
+
+const PostDetails = () => {
+  return (
+    <div>
+      slug
+    </div>
+  )
+}
+
+export default PostDetails
