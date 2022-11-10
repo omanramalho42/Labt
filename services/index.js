0e47d9d8e92aa4cf868320b1ea715626cd940e31
= <=>
2022-11-10 14:19:25 -0300

    configration method to get similar and latest posts

--- services/index.js
+++ services/index.js
@@ -38,3 +38,62 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges;
 }
+
+export const getRecentPost = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
+
+export const getSimilarPost = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetGategories {
+        categories {
+          name
+          slug
+        }
+    }
+  `;
+  
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+};
