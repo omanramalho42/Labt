0610a9b115c0b63846086186d36b31aa48a14c5b
= <=>
2022-11-10 12:12:47 -0300

    building inital structure index app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+const posts = [
+  { title: 'Primeiro Post', excerpt: 'Lorem ipsum dollar sign' },
+  { title: 'Segundo Post', excerpt: 'Lorem ipsum dollar sign' },
+];
+
 const Home: NextPage = () => {
   return (
    <div className='container mx-auto px-10 mb-8'>
@@ -8,7 +13,21 @@ const Home: NextPage = () => {
         <title>Laboratório Temporal</title>
         <link rel="stylesheet" href="favicon.ico" />
       </Head>
-      
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          { posts?.map((i, idx) => (
+            <div key={idx}>
+              { i.title }
+              { i.excerpt }
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              
+            </div>
+        </div>
+      </div>
    </div>
   )
 }
