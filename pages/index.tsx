9e193f9a7c700a852be43e7f2141365fd0883fc8
= <=>
2022-11-10 12:17:50 -0300

    creating, export and import new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import { 
+  PostCard, 
+  PostWidget, 
+  Categories 
+} from '../components'
+
 const posts = [
   { title: 'Primeiro Post', excerpt: 'Lorem ipsum dollar sign' },
   { title: 'Segundo Post', excerpt: 'Lorem ipsum dollar sign' },
@@ -17,14 +23,14 @@ const Home: NextPage = () => {
         <div className='lg:col-span-8 col-span-1'>
           { posts?.map((i, idx) => (
             <div key={idx}>
-              { i.title }
-              { i.excerpt }
+              <PostCard post={i} key={i.title} />
             </div>
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-              
+              <PostWidget />
+              <Categories />
             </div>
         </div>
       </div>
