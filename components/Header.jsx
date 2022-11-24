9cfad3545de39fb7a374c8af7ddde53e663d306f
= <=>
2022-11-24 12:38:03 -0300

    mudando fonte

--- components/Header.jsx
+++ components/Header.jsx
@@ -90,7 +90,7 @@ const Header = () => {
         </div>
         
         <div className='w-full flex align-middle justify-between items-center'>
-          <div className="hidden md:float-right md:contents items-center">
+          <div className="hidden md:float-right md:contents items-center" style={{ fontFamily: 'Arlita'}}>
             <a
               href='/' 
               className='md:float-right flex items-center mt-2 text-black dark:text-white font-semibold cursor-pointer lg:text-2xl'
