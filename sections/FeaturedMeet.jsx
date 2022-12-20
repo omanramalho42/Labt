5a6974f590a5ce7ca2c23631a55e9a336850b392
= <=>
2022-12-20 17:13:35 -0300

    modificando margem v2

--- sections/FeaturedMeet.jsx
+++ sections/FeaturedMeet.jsx
@@ -96,7 +96,7 @@ const FeaturedMeet = () => {
           position: '-webkit-sticky',
           height: '-webkit-fill-available',
           width: '-webkit-fill-available',
-          marginBottom: ios && mobile.innerWidth >= 450 && mobile.innerWidth <= 1200 ? '30em' : '10em',
+          marginBottom: ios && mobile.innerWidth >= 450 && mobile.innerWidth <= 1200 ? '35em' : '10em',
         }}
       >
         <div 
