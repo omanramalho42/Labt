60be7e04aa1778ab99f5346c435a8b4826d8caf9
= <=>
2023-01-03 10:51:06 -0300

    adicioanndo meta ao document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,7 +16,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,500&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
-
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" /> 
           <link rel="stylesheet" href='/icon.png' />
         </Head>
         <body>
